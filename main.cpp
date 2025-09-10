b7c402640773fea7abdbb6530ee074bf7e18b5be
florian <seiffarth@cs.uni-bonn.de>
2025-09-10 17:04:03 +0200

    Refactor main function to enhance graph loading and parallelize GED computation using OpenMP

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <LoadSave.h>
 #include "load_tu.h"
 #include "Algorithms/GED/GEDLIBWrapper.h"
+#include "Algorithms/GED/GEDFunctions.h"
 
 
 int main() {
@@ -29,14 +30,36 @@ int main() {
     }
 
 
-    if (bool success = create_tu("MUTAG", input_path, output_path); !success) {
+    if (bool success = LoadSave::PreprocessTUDortmundGraphData("MUTAG", input_path, output_path); !success) {
         std::cout << "Failed to create TU dataset" << std::endl;
         return 1;
     }
-    GraphData<GraphStruct> graphs = load_tu("MUTAG", output_path);
-    ged::GEDEnv<ged::LabelID, ged::LabelID, ged::LabelID> env;
-    InitializeGEDEnvironment(env, graphs,ged::Options::EditCosts::CONSTANT, ged::Options::GEDMethod::F2);
-    ComputeGEDResults(env, graphs, mapping_path_output);
+    GraphData<GraphStruct> graphs = LoadSave::LoadPreprocessedTUDortmundGraphData("MUTAG", output_path);
+    // set omp number of threads to max threads of this machine
+    int num_threads_used = 4;
+    int x = omp_get_num_threads();
+    int chunk_size = min(30, (int)graphs.graphData.size()/num_threads_used);
+    std::vector<std::pair<INDEX, INDEX>> graph_ids;
+    for (auto i = 0; i < graphs.graphData.size(); ++i) {
+        for ( auto j = i + 1; j < graphs.graphData.size(); ++j) {
+            graph_ids.emplace_back(std::make_pair(i,j));
+        }
+    }
+    std::vector<std::vector<std::pair<INDEX, INDEX>>> graph_id_chunks;
+    // split graph ids into chunks of size chunk_size
+    for (int i = 0; i < graph_ids.size(); i += chunk_size) {
+        graph_id_chunks.push_back(std::vector<std::pair<INDEX, INDEX>>(graph_ids.begin() + i, graph_ids.begin() + min(i + chunk_size, (int)graph_ids.size())));
+    }
+
+
+    // parallelize computation of ged using openmp
+#pragma omp parallel for schedule(dynamic) shared(graphs, graph_id_chunks, mapping_path_output) default(none) num_threads(num_threads_used)
+    for (const auto & graph_id_chunk : graph_id_chunks) {
+        ged::GEDEnv<ged::LabelID, ged::LabelID, ged::LabelID> env;
+        InitializeGEDEnvironment(env, graphs, ged::Options::EditCosts::CONSTANT, ged::Options::GEDMethod::F2);
+        ComputeGEDResults(env, graphs, graph_id_chunk, mapping_path_output);
+    }
+    MergeGEDResults(mapping_path_output, graphs);
     // load mappings
     std::vector<GEDEvaluation> results;
     BinaryToGEDResult(mapping_path_output + "MUTAG_ged_mapping.bin", graphs, results);
